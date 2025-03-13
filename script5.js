750a718c857ed2c60ea7feb83074eae666ea4abb
GambleLM9 <profgyr0@gmail.com>
2025-03-13 14:25:07 -0700

    Removed Logging Tools

--- script5.js
+++ script5.js
@@ -5,14 +5,14 @@ let counter = 0//Variable for the score counter
 let cheeseCounter = 0 //Variable for the anti-cheat
 let cheeseTimer = ''//Variable for the anti-cheat timer
 function beginGame(){//Begins the game on activation
-    console.log("Game started")//LOGGING: Logs the message in the console, signaling that the game has began
+    // console.log("Game started")//LOGGING: Logs the message in the console, signaling that the game has began
     timerStart()//Activates the timerStart function
 }
 function timerStart(){//Starts the timer on activation
     let interval = Math.floor(Math.random() * (61 - 1)) + 1//Picks a random number between 60 and 1, and rounds it down.
-    console.log(interval.toFixed() + " Seconds")//LOGGING: Logs the randomized time on the console
+    // console.log(interval.toFixed() + " Seconds")//LOGGING: Logs the randomized time on the console
     let timervalue = interval * 1000//Multiplies the random number by 1000 to convert it into miliseconds.
-    console.log(timervalue)//LOGGING: Logs the result of timervalue in the console.
+    // console.log(timervalue)//LOGGING: Logs the result of timervalue in the console.
     timer = setTimeout(displayButton, timervalue.toFixed()) //Sets a time out, activating the displayButton function after the randomized timervalue has passed.
 }
 function displayButton(){//Displays the button on activation
@@ -24,7 +24,7 @@ function displayButton(){//Displays the button on activation
 }
 function gameLost(){//Displays the game over message on activation
     theButton.innerHTML = ''//Clears the DOM for the Button holder, making the button disappear.
-    console.log("Oops! Game over!")//LOGGING: Logs the message in the console to signal a game over
+    // console.log("Oops! Game over!")//LOGGING: Logs the message in the console to signal a game over
     //Sets the DOM for the Button holder equivalent to the following code, generating a game over message.
     theButton.innerHTML += `
         <div class="gameover">
@@ -35,7 +35,7 @@ function gameLost(){//Displays the game over message on activation
 }
 function buttonClick(){//Clears the activation timer and awards a point upon activation
     clearTimeout(timer)//Clear the activation timer
-    console.log("Congrats!")//LOGGING: Logs the message in the console, signaling that the round has been completed.
+    // console.log("Congrats!")//LOGGING: Logs the message in the console, signaling that the round has been completed.
     theButton.innerHTML = ''//Clears the DOM for the Button holder, making the button disappear.
     counter++//Increments the score count by 1
     scoreboard.innerHTML = counter//Sets the DOM for the scoreboard equal to the value of the counter
@@ -44,15 +44,15 @@ function buttonClick(){//Clears the activation timer and awards a point upon act
 function tryAgain(){//Resets the game and restarts it upon activation
     theButton.innerHTML = ''//Clears the DOM for the Button holder, making the button disappear.
     counter = 0//Resets the score to 0
-    console.log(counter, "Counter")//LOGGING: Logs the value of the counter to confirm a reset
+    // console.log(counter, "Counter")//LOGGING: Logs the value of the counter to confirm a reset
     scoreboard.innerHTML = counter//Sets the DOM for the scoreboard equal to the value of the counter, which should be 0
     timerStart()//Activates the timerStart() function to begin the game again
 }
 function mouseTrap(){ //This is the game's anti-cheat system
     cheeseCounter++ //Adds 1 to the anti-cheat counter
-    console.log('Cheesy', cheeseCounter) //LOGGING: Logs the current value of the anti-cheat counter
+    // console.log('Cheesy', cheeseCounter) //LOGGING: Logs the current value of the anti-cheat counter
     if(cheeseCounter > 5){ //Checks if the anti-cheat counter exceeds 5
-        console.log('Trap Activated!') //LOGGING: Logs the activation of the anti-cheat
+        // console.log('Trap Activated!') //LOGGING: Logs the activation of the anti-cheat
         clearTimeout(timer) //Clears the current timer
         theButton.innerHTML='' //Clears the DOM for the Button holder, making the button disappear.
         //Sets the DOM for the Button holder equivalent to the following code, generating a pop up to call the player out for cheating
